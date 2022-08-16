dbc2bc4cc4ce057c2e5d715f380719cdc0f511fe
Yassin Yahyawi <96199527+Yahyawi99@users.noreply.github.com>
2022-08-16 23:36:13 +0100

    fix bugs

---
+++ components/shared/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+// css
+import footerStyles from "../../styles/shared/Footer.module.css";
+
+const Footer = () => {
+  return <footer className={footerStyles.container}>FOOTER</footer>;
+};
+
+export default Footer;
